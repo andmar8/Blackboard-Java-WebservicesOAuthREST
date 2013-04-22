6b8d13249eae2c59e9314e11926374ed88b2e56d
Andrew Martin <andrew.martin@ncl.ac.uk>
2013-04-22 12:16:41 +0100

    * Initial source release

---
+++ src/bbws/util/security/oauth/impl/ServerImpl.java
@@ -0,0 +1,92 @@
+/*
+    Blackboard Webservices OAuth REST Implementation
+    Copyright (C) 2011-2013 Andrew Martin, Newcastle University
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */package bbws.util.security.oauth.impl;
+
+//bbws
+import bbws.util.security.oauth.Server;
+
+//java
+import java.util.Calendar;
+
+//bbws
+import bbws.util.security.oauth.DefaultOAuth;
+
+////jersey
+//import com.sun.jersey.oauth.signature.OAuthParameters;
+//import com.sun.jersey.oauth.signature.OAuthSecrets;
+
+public class ServerImpl extends DefaultOAuth implements Server
+{
+    public ServerImpl(String baseuri,String consumer_key,String consumer_secret)
+    {
+        if(!baseuri.endsWith("/")){this.baseuri = baseuri+"/";}
+        else{this.baseuri = baseuri;}
+        this.consumer_key = consumer_key;
+        this.consumer_secret = consumer_secret;
+    }
+//
+//    public boolean isRequestValid(long requestTimeout,String requestMethod,String resource,OAuthParameters requestParams)
+//    {
+//        //check nonce
+//        //nonceHasBeenUsedRecently(nonce,requestTimeout);
+//
+//        //check timestamp occured before timeout or after current time, reject request if true
+//        long timestamp = Long.parseLong(requestParams.getTimestamp());
+//        long currentTimestamp = Calendar.getInstance().getTimeInMillis()/1000;
+//        if(timestamp<(Calendar.getInstance().getTimeInMillis()/1000-requestTimeout)||timestamp>currentTimestamp)
+//        {
+//            return false;
+//        }
+//
+//        //check signature
+//        try
+//        {
+//            //return isSignatureValid(requestMethod,this.baseuri+resource,this.consumer_key,this.consumer_secret,requestParams.getNonce(),Long.toString(timestamp),requestParams.getSignature());
+//            return isSignatureValid(new Request("GET",this.baseuri+resource,requestParams),requestParams,new OAuthSecrets().consumerSecret(this.consumer_secret));
+//        }
+//        catch(Exception e)
+//        {
+//            System.out.println("Error checking signature: "+e.getMessage());
+//        }
+//        return false;
+//    }
+
+    @Override
+    public boolean isRequestValid(long requestTimeout,String requestMethod,String resource,String nonce,String signature,long timestamp)
+    {
+        //check nonce
+        //nonceHasBeenUsedRecently(nonce,requestTimeout);
+
+        //check timestamp occured before timeout or after current time, reject request if true
+        long currentTimestamp = Calendar.getInstance().getTimeInMillis()/1000;
+        if(timestamp<(Calendar.getInstance().getTimeInMillis()/1000-requestTimeout)||timestamp>currentTimestamp)
+        {
+            return false;
+        }
+
+        //check signature
+        try
+        {
+            return isSignatureValid(requestMethod,this.baseuri+resource,this.consumer_key,this.consumer_secret,nonce,Long.toString(timestamp),signature);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error checking signature: "+e.getMessage());
+        }
+        return false;
+    }
+}
